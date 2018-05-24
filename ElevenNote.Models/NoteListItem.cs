baf4387ae8f5f040b28190c86822daed11108455
Nicole jeffries <nicolejeffries0118@gmail.com>
2018-05-24 09:56:30 -0400

    ElevenNote

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+   public class NoteListItem
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
